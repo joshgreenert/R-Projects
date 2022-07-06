2ccf066932bdaf4c91220bcbe0109b08d526c405
Joshua Greenert <42500582+joshgreenert@users.noreply.github.com>
2022-07-06 19:53:25 -0400

    Completed first assignment

---
+++ Statistics For Data Science/Assignments/assignment_5_Tranformations_GreenertJoshua.R
@@ -0,0 +1,17 @@
+# Assignment: ASSIGNMENT 5
+# Name: Greenert, Joshua
+# Date: 7/5/2022
+'''
+Using either the same dataset(s) you used in the previous weeks’ exercise or a brand-new dataset
+of your choosing, perform the following transformations
+'''
+# 1.Using the dplyr package, use the 6 different operations to analyze/transform the data - GroupBy, Summarize, Mutate, 
+#   Filter, Select, and Arrange – Remember this isn’t just modifying data, you are learning about your data also – so 
+#   play around and start to understand your dataset in more detail
+
+
+# 2.Using the purrr package – perform 2 functions on your dataset.  You could use zip_n, keep, discard, compact, etc.
+
+# 3.Use the cbind and rbind function on your dataset
+
+# 4.Split a string, then concatenate the results back together
