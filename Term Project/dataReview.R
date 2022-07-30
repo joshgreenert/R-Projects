e47cc7f9411c3fbedec338c86dcb21d8d344a185
Joshua Greenert <42500582+joshgreenert@users.noreply.github.com>
2022-07-30 17:51:36 -0400

    Upload deliverable

---
+++ Term Project/dataReview.R
@@ -0,0 +1,9 @@
+# Assignment: Term Project Spelunking
+# Name: Greenert, Joshua
+# Date: 7/30/2022
+
+## Set the working directory to the root of your DSC 520 directory
+setwd("C:/Users/Josh/Documents/GitHub/R-Projects/Statistics For Data Science")
+
+
+
