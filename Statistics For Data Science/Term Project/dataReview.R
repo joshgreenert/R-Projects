859082704a638735c6714d876059adf659c61a68
Joshua Greenert <42500582+joshgreenert@users.noreply.github.com>
2022-08-07 15:05:55 -0400

    Push over data

--- Statistics For Data Science/Term Project/dataReview.R
+++ Statistics For Data Science/Term Project/dataReview.R
@@ -3,7 +3,18 @@
 # Date: 7/30/2022
 
 ## Set the working directory to the root of your DSC 520 directory
-setwd("C:/Users/Josh/Documents/GitHub/R-Projects/Statistics For Data Science")
+setwd("C:/Users/Josh/Documents/GitHub/R-Projects/Statistics For Data Science/Term Project")
+
+homePriceIndexDF <- read.csv("HPI_master.csv")
+minimumWageDF <- read.csv("Minimum Wage Data.csv")
+dollarHistoryDF <- read.csv("US Dollar Index Historical Data.csv")
+
+#ggplot(minimumWageDF, aes(x = Year, y = Effective.Minimum.Wage, col = State)) + geom_point() + geom_smooth()
 
 
+#ggplot(homePriceIndexDF, aes(x = yr, y = index_sa, col = period)) + geom_point() +
+#  geom_smooth() + ggtitle("Home Price Index") + xlab("Year") + ylab("NSA")
 
+
+ggplot(dollarHistoryDF, aes(x = yr, y = index_sa, col = period)) + geom_point() +
+  geom_smooth() + ggtitle("Home Price Index") + xlab("Year") + ylab("NSA")
